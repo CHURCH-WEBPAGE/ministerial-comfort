e29723f6a2a8bd854c8674053bbb49949debbae4
olu-martins <mayowasamuel86@gmail.com>
2025-11-22 13:28:26 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Ministerial Comfort and Renewal (MCR)",
   description: "Restoring Hope and Renewal for Ministers",
+  icons: {
+    icon: "/assets/MCR logo 2.svg",
+    shortcut: "/assets/MCR logo 2.svg",
+    apple: "/assets/MCR logo 2.svg",
+  },
 };
 
 export default function RootLayout({
