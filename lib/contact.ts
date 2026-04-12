3c2a04df44003ee8f12e1b7a867d16684d8fd5d4
olu-martins <mayowasamuel86@gmail.com>
2026-04-12 02:22:11 +0100

    add donate

---
+++ lib/contact.ts
@@ -0,0 +1,9 @@
+/** Shared contact copy used across Footer, Contact, etc. */
+export const CONTACT_ADDRESS =
+  'Foursquare Gospel Church, Victoria Garden City, Lagos Nigeria';
+
+export const SUPPORT_EMAIL = 'support@foursquaremcr.org';
+
+export const SUPPORT_PHONE_DISPLAY = '+234-1-7317138';
+
+export const SUPPORT_PHONE_TEL = '+23417317138';
---
+++ lib/donate.ts
@@ -0,0 +1,9 @@
+export const DONATE_ACCOUNT_NAME =
+  'Foursquare Gospel church in Nigeria- Ministerial Comfort and Renewal (MCR)';
+
+/** Digits only for copy / clipboard */
+export const DONATE_ACCOUNT_NUMBER_RAW = '0126642209';
+
+export const DONATE_ACCOUNT_NUMBER_DISPLAY = '012 664 2209';
+
+export const DONATE_BANK_NAME = 'Wema Bank';
